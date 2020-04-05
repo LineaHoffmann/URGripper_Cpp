7172f1708625e09741e45223afc9b8382e42fbd8
LineaHoffmann <Linea.hoffmann.christophersen@gmail.com>
2020-04-05 22:54:51 +0200

    Added Wheatstone class

---
+++ URGripperCpp/wheatstone.cpp
@@ -0,0 +1,6 @@
+#include "wheatstone.h"
+
+Wheatstone::Wheatstone()
+{
+
+}
---
+++ URGripperCpp/wheatstone.h
@@ -0,0 +1,11 @@
+#ifndef WHEATSTONE_H
+#define WHEATSTONE_H
+
+
+class Wheatstone
+{
+public:
+    Wheatstone();
+};
+
+#endif // WHEATSTONE_H
