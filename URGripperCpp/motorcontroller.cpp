51b9be76e14d21293fd287cfe915be4e8ff747f4
Jonas Sander <jonas.sander171@gmail.com>
2020-04-07 14:02:17 +0200

    added potmeter
    and motorcontroller

--- URGripperCpp/motorcontroller.cpp
+++ URGripperCpp/motorcontroller.cpp
@@ -20,23 +20,72 @@ MotorController::MotorController(L298 driver, ADC0832 potAdc, ADC0832 forceAdc) 
 
 bool MotorController::move(unsigned int toPosition, unsigned int force)
 {
+    if (toPosition > 0 && toPosition <=100) {
+        if (this->calcForce() <= maxForce) {
+            if (this->calcPosition() == 1000) {
+                std::cout << "error in potmeter" << std::endl;
+                return 1;
+            }
 
+            while (position != toPosition){
+
+                if (position < toPosition) {
+                    if (this->calcForce() > force) {
+                        return 1;
+                    }
+                    //kode til motor her:
+
+
+
+
+
+
+                } else if (position > toPosition) {
+                    if (this->calcForce() > force) {
+                        return 1;
+                    }
+                    //kode til motor her:
+
+
+
+
+                }
+
+            }
+
+        } else {
+            std::cout << "error in force" << std::endl;
+            return 1;
+}
+    }
+    else {
+       std::cout << "error in position" << std::endl;
+       return 1;
+    }
+    return 0;
 }
 
 unsigned int MotorController::calcPosition()
 {
-    position = ((this->getPotReading(1) - 0) * (100 - 0) / (256 - 0)) + 0;
+    unsigned int potReading = this->getPotReading(1);
+    if (potReading == 1000) {
+        //error in reading
+        return 1000;
+    } else {
+    //output = output_start + ((output_end - output_start) / (input_end - input_start)) * (input - input_start)
+    position = 0 + ((100 - 0) / (potRange[1] - potRange[0])) * (potReading - 0);
     return position;
+    }
 }
 
 unsigned int MotorController::getPotReading(unsigned int channel)
 {
     unsigned int tempReading = _potAdc.readADC(channel);
-    if (tempReading <= 256 && tempReading >= potRange[0] && tempReading <= potRange[1]) {
+    if (tempReading < 256 && tempReading >= potRange[0] && tempReading <= potRange[1]) {
         return tempReading;
     } else {
         std::cout << "pot reading not in accepted range" << std::endl;
-        return 0;
+        return 1000;
     }
 }
 
